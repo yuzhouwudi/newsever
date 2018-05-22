f969de98b5652d7e2e60623fc82f85bea0182012
weirdo <1061420168qq.com>
2018-05-22 22:29:48 +0800

    penautsever

---
+++ mysqls/pool.js
@@ -0,0 +1,42 @@
+/**
+ * Created by ow on 2018/4/28.
+ */
+
+
+
+let mysql=require('mysql')
+const option={
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'',
+    database:'penaut'
+}
+
+let pool=mysql.createPool(option)
+
+// 获取空闲连接接口
+function query(str,callback) {
+    pool.getConnection(function (err,sql) {
+        if(err){
+            callback(err)
+        }else{
+            sql.query(str,function (err,res) {
+                if(err){
+                    callback(err)
+                }else {
+                    callback(null,res);
+                    sql.release();
+                }
+
+            })
+        }
+
+    })
+
+}
+
+
+
+module.exports=query
+
