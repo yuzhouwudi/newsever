5feb3654a6c2e886b7018455c33ff6d862a0347f
weirdo <1061420168qq.com>
2018-05-23 10:31:56 +0800

    'penautsever'

--- routes/admin/manager.js
+++ routes/admin/manager.js
@@ -7,6 +7,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     if(!req.session.login){
         res.send('no')
+    }else{
+        res.send(req.session.user)
+    }
+
+});
+
+router.get('/clear', function(req, res, next) {
+    if(req.session.login){
+        // delete req.session.user;
+        // req.session.login='no'
+        res.send('edit')
     }
 
 });
