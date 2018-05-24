b3dd759b907f9228b522b94a6b37dde680661c41
weirdo <1061420168qq.com>
2018-05-24 22:51:11 +0800

    'penautsever'

--- routes/admin/goods.js
+++ routes/admin/goods.js
@@ -12,6 +12,7 @@ let upload = multer({
     dest: os.tmpdir()
 })
 
+// 坚果分类
 /* GET home page. */
 router.get('/nut', function (req, res) {
     query('select * from product where fid=2', function (err, sql) {
@@ -29,27 +30,98 @@ router.get('/nutmod', function (req, res) {
     })
 });
 
+router.get('/del', function (req, res) {
+    let id = req.query.id
+    query('delete from product where id=' + id, function (err, sql) {
+        if (err) throw err;
+        res.send(sql);
+    })
+});
+
 
 router.post('/upload', upload.single('file'), function (req, res, next) {
     res.send(req.file.path)
 });
 
-router.post('/form', function (req, res) {
-    let body = req.body
-    let img = JSON.parse(body.fileList)
+
+// 转存
+function uploading(pic) {
+    let img = JSON.parse(pic)
     let str = []
     img.forEach(val => {
         if (val.response) {
             let newpath = "/" + Date.now() + val.name
-            fs.renameSync(val.response, './public' + newpath)
-            str.push({name: val.name, url: newpath})
+            // fs.renameSync(val.response, './public' + newpath)
+            // 跨盘转存
+            let input = fs.createReadStream(val.response);
+            let output = fs.createWriteStream('./public/img' + newpath);
+            input.pipe(output, function () {
+                fs.unlinkSync(val.response);
+                // 删除临时文件
+            });
+            str.push({name: val.name, url: '/api/img'+newpath})
         } else {
             str.push({name: val.name, url: val.url})
         }
     })
-    console.log(str);
-    res.send(str)
+    return JSON.stringify(str)
+}
+
+
+// 修改
+router.post('/upd', function (req, res) {
+    let id = req.query.id
+    let body = req.body
+    let imgstr = uploading(body.img)
+    query(`update product set name='${body.name}',fid='${body.fid}',rid='${body.rid}'
+     ,price='${body.price}',count='${body.count}',contfree='${body.contfree}',des='${body.des}',
+     img='${imgstr}',time='${body.time}' where id=${id}`, function (err, data) {
+        if (err) throw err
+        if (data.affectedRows === 1) {
+            res.send('ok')
+        } else {
+            res.send('no')
+        }
+    })
 
 });
+
+
+router.get('/nutfenlei', function (req, res) {
+    query('select * from fenlei', function (err, sql) {
+        if (err) throw err;
+        res.send(sql);
+    })
+});
+
+router.get('/nutrecom', function (req, res) {
+    query('select * from recom', function (err, sql) {
+        if (err) throw err;
+        res.send(sql);
+    })
+});
+
+// 添加
+router.post('/add', function (req, res) {
+    let body = req.body
+
+    let imgstr = uploading(body.img)
+
+
+    query(`insert into product (name,fid,rid,price,count,contfree,des,img,time) 
+        values ('${body.name}','${body.fid}','${body.rid}','${body.price}',
+        '${body.count}','${body.contfree}','${body.des}','${imgstr}','${body.time}')`, function (err, data) {
+        if (err) throw err
+        if (data.affectedRows === 1) {
+            res.send('ok')
+        } else {
+            res.send('no')
+        }
+
+    })
+
+
+});
+
 
 module.exports = router;
